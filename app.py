1ec860baae1cec202484046ef1516f840323ee8c
Ilayda Cavusoglu Pars <ilayda.cavusoglupars@canonical.com>
2026-02-24 11:03:17 +0300

    add a temp endpoint to see headers

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import logging
 from flask import Flask, jsonify, render_template, redirect, session, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_limiter import Limiter
-from flask_limiter.util import get_remote_address
+from flask import request as flask_request
 from flask_migrate import Migrate
 from sqlalchemy import text
 from config import Config
@@ -15,7 +15,14 @@ logging.basicConfig(
 
 db = SQLAlchemy()
 migrate = Migrate()
-limiter = Limiter(key_func=get_remote_address, default_limits=[])
+def get_client_ip():
+    forwarded = flask_request.headers.get("X-Forwarded-For", "")
+    if forwarded:
+        return forwarded.split(",")[0].strip()
+    return flask_request.remote_addr
+
+
+limiter = Limiter(key_func=get_client_ip, default_limits=[])
 
 
 class ReverseProxied:
@@ -61,6 +68,16 @@ def create_app(config_class=Config):
         if "user" not in session:
             return redirect(url_for("login", next=url_for("dashboard")))
         return render_template("dashboard.html", user=session["user"])
+
+    # TODO: Remove this endpoint after verifying proxy headers
+    @app.route("/debug-headers")
+    def debug_headers():
+        return jsonify({
+            "remote_addr": flask_request.remote_addr,
+            "x_forwarded_for": flask_request.headers.get("X-Forwarded-For"),
+            "x_real_ip": flask_request.headers.get("X-Real-Ip"),
+            "resolved_client_ip": get_client_ip(),
+        })
 
     @app.route("/db-test")
     def dbtest():
